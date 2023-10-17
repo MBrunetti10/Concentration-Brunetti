5d268c361fd03db968a8cb618c9d5eaf7cbeae0b
MBrunetti10 <mbrunetti88@gmail.com>
2023-10-16 22:44:09 -0400

    about 99% done. worked on the function of winning/losing the game

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 //Cards
 const cards = document.querySelectorAll('.memory-card');
+const winningHeading = document.getElementById('Winning-heading');
+const losingHeading = document.getElementById('Losing-heading');
 // now i need to make an object that'll equal of flipping the card.
 // put the guesses in the array.
 
+//store a guess
 let guesses = []
+let moves = 0;
+let correctMatches = 0;
+let win = false;
+
 
 cards.forEach((card) => {
     card.addEventListener("click", (event) => {
         //checking to see if we are targeting the image.
-        if (event.target.classList.contains("front-face")) {
+     if (moves <= 29){
+
+     
+        if (event.target.classList.contains("front-face")|| event.target.classList.contains("back-face"))  {
             if (!event.target.parentElement.classList.contains("active")) {
                 event.target.parentElement.classList.add("active")
                 guesses.push(event.target.parentElement.dataset.card)
-                if (guesses.length === 1) {
+                
                     event.target.parentElement.classList.add("pending")
-                }
+                
             }
 
         } else {
@@ -24,28 +34,44 @@ cards.forEach((card) => {
                 event.target.classList.add("pending")
 
             }
-        }
+        }}
 
         if (guesses.length === 2) {
             if (guesses[0] === guesses[1]) {
-                document.querySelector.add('.pending').forEach((card) => {
+                document.querySelectorAll('.pending').forEach((card) => {
                     card.classList.add("succesful")
                     card.classList.remove("pending")
 
-                })
+                }) 
+                correctMatches +=1;
             } else {
-                document.querySelector.add('.pending').forEach((card) => {
+                document.querySelectorAll('.pending').forEach((card) => {
                     card.classList.add("wrong")
                     card.classList.remove("pending")
                     setTimeout(function () {
-                        document.querySelector.add('.wrong').forEach((card) => {
+                        document.querySelectorAll('.wrong').forEach((card) => {
                             card.classList.remove("active")
                             card.classList.remove("wrong")
                         })
                     }, 500);
                 })
             }
+            guesses.pop();
+            guesses.pop();
+            moves +=1;
+            //winning and losing logic
+            if(correctMatches === 8){
+                win = true
+            winningHeading.style.display = "block"    
+            }
+            if(moves === 30 && win===false ){
+            losingHeading.style.display = "block"
+            }
         }
+
+
+
+        
     })
 })
 
